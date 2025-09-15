5f462602ad854d65a80d17123c9923d6f9581c16
Andre Freitas <andrecfreitas58@gmail.com>
2025-09-15 16:02:49 +0100

    fix(zsh): oh-my-posh prompt

--- .zsh/prompt/oh-my-posh/init.zsh
+++ .zsh/prompt/oh-my-posh/init.zsh
@@ -5,18 +5,23 @@
   local command=${commands[oh-my-posh]:-"$(${commands[mise]} which oh-my-posh 2> /dev/null)"}
   [[ -z $command ]] && return 1
 
+  local theme
+  zstyle -a ":user:prompt:theme" theme "theme"
+  local config="$HOME/.config/ohmyposh/$theme.yaml"
+
   # generating init file
   local initfile=$1/oh-my-posh-init.zsh
   if [[ ! -e $initfile || $initfile -ot $command || $initfile -ot $HOME/.zshrc ]]; then
-    local theme
-    zstyle -a ":user:prompt:theme" theme "theme"
-    local config="$HOME/.config/ohmyposh/$theme.yaml"
     if [[ -f $config ]]; then
       $command init zsh --config $config >| $initfile
     else
       $command init zsh >| $initfile
     fi
     print -u2 -PR "* [oh-my-posh] Detected a change to .zshrc. Regenerated init file."
+    zcompile -UR $initfile
+  else if [[ -f $config && $initfile -ot $config ]]; then
+    $command init zsh --config $config >| $initfile
+    print -u2 -PR "* [oh-my-posh] Detected a change to config. Regenerated init file."
     zcompile -UR $initfile
   fi
 
