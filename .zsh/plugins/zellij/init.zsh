516436e56746ffd9d5bdb3106cf78df29a2ada4e
Andre Freitas <andrecfreitas58@gmail.com>
2025-09-26 14:19:51 +0100

    feat(zsh): add zellij config

---
+++ .zsh/plugins/zellij/init.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+(( ${+commands[zellij]} || ${+commands[mise]} )) && () {
+  local command=${commands[zellij]:-"$(${commands[zellij]} which zellij 2> /dev/null)"}
+  [[ -z $command ]] && return 1
+
+  # generating completions
+  local compfile=$1/functions/_zellij
+  if [[ ! -e $compfile || $compfile -ot $command ]]; then
+    $command setup --generate-completion zsh >| $compfile
+    print -u2 -PR "* [zellij] Detected a new version 'zellij'. Regenerated completions."
+  fi
+
+  alias zj="$command"
+  alias zja="$command a"
+
+} ${0:h}
+
