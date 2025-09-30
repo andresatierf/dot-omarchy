6e9346a47a1c973273ce0fb5710846e66a16ef14
Andre Freitas <andrecfreitas58@gmail.com>
2025-09-30 14:43:50 +0100

    feat(zsh): add missing fzf shell integration

--- .zsh/plugins/fzf/init.zsh
+++ .zsh/plugins/fzf/init.zsh
@@ -10,6 +10,14 @@
 
   local lowest_version="$(printf '%s\n' "$max_version" "$curr_version" | sort -V | head -n1)"
 
+  # generating initfile
+  local initfile=$1/fzf-init.zsh
+  if [[ ! -e $initfile || $initfile -ot $command ]]; then
+    $command --zsh >| $initfile
+    zcompile -UR $initfile
+    print -u2 -PR "* [fzf] Generated init file."
+  fi
+
   if [[ $lowest_version != "$max_version" ]]; then
     # generating completions
     source /usr/share/doc/fzf/examples/key-bindings.zsh
@@ -18,4 +26,5 @@
 
   export FZF_DEFAULT_OPTS='--height=20 --layout=reverse --ansi --color "fg:-1"'
 
+  source $initfile
 } ${0:h}
